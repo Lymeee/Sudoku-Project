71c4792782060cf705462b945fe77f039625289d
Lymee <slimylymee@gmail.com>
2024-08-02 14:04:41 -0400

    made sudoku_gui.py

---
+++ sudoku_gui.py
@@ -0,0 +1,22 @@
+import pygame, copy
+import sys
+from sudoku_generator import *
+
+
+
+class Board:
+    def __init__(self, width, height, screen, difficulty):
+    def generate_board(self):
+    def is_full(self):
+    def draw(self):
+    def select(self, row, col):
+    def click(self, x, y):
+    def arrows(self, direction):
+    def place_number(self, value):
+    def reset_to_original(self):
+    def check_win(self):
+class Cell:
+    def __init__(self, value, row, col, width, height):
+    def set_cell_value(self, value):
+    def draw(self):
+def draw_button(screen, button, button_name):
