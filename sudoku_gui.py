d0a03e984995f76d30293272ee1f56d4b084cc7f
Lymee <slimylymee@gmail.com>
2024-08-02 14:07:57 -0400

    pass

--- sudoku_gui.py
+++ sudoku_gui.py
@@ -6,17 +6,32 @@ from sudoku_generator import *
 
 class Board:
     def __init__(self, width, height, screen, difficulty):
+        pass
     def generate_board(self):
+        pass
     def is_full(self):
+        pass
     def draw(self):
+        pass
     def select(self, row, col):
+        pass
     def click(self, x, y):
+        pass
     def arrows(self, direction):
+        pass
     def place_number(self, value):
+        pass
+
     def reset_to_original(self):
+        pass
     def check_win(self):
+        pass
 class Cell:
     def __init__(self, value, row, col, width, height):
+        pass
     def set_cell_value(self, value):
+        pass
     def draw(self):
+        pass
 def draw_button(screen, button, button_name):
+    pass
