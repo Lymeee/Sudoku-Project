0e2fdcc01ddcdc7262685dc1e0d92cd09c198e09
Lymee <slimylymee@gmail.com>
2024-08-02 14:34:07 -0400

    Valids and generate board

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -1,4 +1,4 @@
-import math,random
+import math,random,copy
 
 """
 This was adapted from a GeeksforGeeks article "Program for Sudoku Generator" by Aarti_Rathi and Ankur Trisal
@@ -23,7 +23,10 @@ class SudokuGenerator:
 	None
     '''
     def __init__(self, row_length, removed_cells):
-        pass
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+        self.board = [[0 for _ in range(row_length)] for _ in range(row_length)]
+        self.box_length = int(math.sqrt(row_length))
 
     '''
 	Returns a 2D python list of numbers which represents the board
@@ -32,7 +35,7 @@ class SudokuGenerator:
 	Return: list[list]
     '''
     def get_board(self):
-        pass
+        return self.board
 
     '''
 	Displays the board to the console
@@ -42,7 +45,10 @@ class SudokuGenerator:
 	Return: None
     '''
     def print_board(self):
-        pass
+        for row in self.board:
+            for num in row:
+                print(num)
+            print()
 
     '''
 	Determines if num is contained in the specified row (horizontal) of the board
@@ -55,7 +61,10 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_row(self, row, num):
-        pass
+        for current_row in range(self.row_length):
+            if self.board[current_row][col] == num:
+                return False
+        return True
 
     '''
 	Determines if num is contained in the specified column (vertical) of the board
@@ -68,7 +77,11 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_col(self, col, num):
-        pass
+        for row in range(row_start, row_start + 3):
+            for col in range(col_start, col_start + 3):
+                if self.board[row][col] == num:
+                    return False
+        return True
 
     '''
 	Determines if num is contained in the 3x3 box specified on the board
@@ -83,7 +96,11 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_box(self, row_start, col_start, num):
-        pass
+        for row in range(row_start, row_start + 3):
+            for col in range(col_start, col_start + 3):
+                if self.board[row][col] == num:
+                    return False
+        return True
     
     '''
     Determines if it is valid to enter num at (row, col) in the board
@@ -96,7 +113,13 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def is_valid(self, row, col, num):
-        pass
+        box_row_start = row - row % self.box_length
+        box_col_start = col - col % self.box_length
+        return all([
+            self.valid_in_row(row, num),
+            self.valid_in_col(col, num),
+            self.valid_in_box(box_row_start, box_col_start, num)
+        ])
 
     '''
     Fills the specified 3x3 box with values
--- sudoku_gui.py
+++ sudoku_gui.py
@@ -2,13 +2,28 @@ import pygame, copy
 import sys
 from sudoku_generator import *
 
-
+board_rows = 9
+line_color = 52, 110, 235
+screen_height = 900 + 100
+screen_width = 900
+square_size = 100
 
 class Board:
     def __init__(self, width, height, screen, difficulty):
-        pass
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.difficulty = difficulty
+        self.board = None
+        self.cells = None
+        self.cells_copy = None
+        self.selected_cell = (0, 0)
     def generate_board(self):
-        pass
+        self.board, self.cells = generate_sudoku(9, self.difficulty)
+        self.cells = [
+            [Cell(self.cells[i][j], i, j, square_size, square_size) for j in range(board_rows)]
+            for i in range(board_rows)
+        ]
     def is_full(self):
         pass
     def draw(self):
@@ -28,7 +43,12 @@ class Board:
         pass
 class Cell:
     def __init__(self, value, row, col, width, height):
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
     def set_cell_value(self, value):
         pass
     def draw(self):
